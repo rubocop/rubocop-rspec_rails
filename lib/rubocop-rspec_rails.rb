e09d445dfbe1a5f683d67a47eeda27b5a190ced8
ydah <t.yudai92@gmail.com>
2024-03-30 08:18:32 +0900

    Fix an error for `RSpecRails/HttpStatus`
    Fix: rubocop/rubocop-rspec_rails#9

--- lib/rubocop-rspec_rails.rb
+++ lib/rubocop-rspec_rails.rb
@@ -4,10 +4,14 @@ require 'pathname'
 require 'yaml'
 
 require 'rubocop'
-require 'rubocop-rspec'
+
+require 'rubocop/rspec/language/node_pattern'
+
+require 'rubocop/rspec/language'
 
 require_relative 'rubocop/rspec_rails/version'
 
+require 'rubocop/cop/rspec/base'
 require_relative 'rubocop/cop/rspec_rails_cops'
 
 project_root = File.join(__dir__, '..')
--- lib/rubocop/cop/rspec_rails_cops.rb
+++ lib/rubocop/cop/rspec_rails_cops.rb
@@ -2,12 +2,8 @@
 
 require_relative 'rspec_rails/avoid_setup_hook'
 require_relative 'rspec_rails/have_http_status'
-require_relative 'rspec_rails/negation_be_valid'
-begin
-  require_relative 'rspec_rails/http_status'
-rescue LoadError
-  # RSpecRails/HttpStatus cannot be loaded if rack/utils is unavailable.
-end
+require_relative 'rspec_rails/http_status'
 require_relative 'rspec_rails/inferred_spec_type'
 require_relative 'rspec_rails/minitest_assertions'
+require_relative 'rspec_rails/negation_be_valid'
 require_relative 'rspec_rails/travel_around'
