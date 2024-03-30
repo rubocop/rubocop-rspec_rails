b4f25a45fe6913b0f64ce0d730291854f5cb8145
ydah <t.yudai92@gmail.com>
2024-03-31 02:01:12 +0900

    Fix an error for unrecognized cop or department `RSpecRails/HttpStatus` when also using rubocop-rails
    Fix: https://github.com/rubocop/rubocop-rspec_rails/issues/8

--- lib/rubocop-rspec_rails.rb
+++ lib/rubocop-rspec_rails.rb
@@ -16,3 +16,20 @@ require_relative 'rubocop/cop/rspec_rails_cops'
 
 project_root = File.join(__dir__, '..')
 RuboCop::ConfigLoader.inject_defaults!(project_root)
+
+# FIXME: This is a workaround for the following issue:
+# https://github.com/rubocop/rubocop-rspec_rails/issues/8
+module RuboCop
+  module Cop
+    class AmbiguousCopName # rubocop:disable Style/Documentation
+      prepend(Module.new do
+        def qualified_cop_name(name, path, warn: true)
+          return super unless name == 'RSpec/Rails/HttpStatus'
+
+          badge = Badge.parse(name)
+          resolve_badge(badge, qualify_badge(badge).first, path)
+        end
+      end)
+    end
+  end
+end
